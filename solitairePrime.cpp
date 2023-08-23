51e2800199cbd300cd2dfa12e45a3e31a5b3aa02
Chris <42982458+Crisu07@users.noreply.github.com>
2023-08-23 02:14:59 -0700

    Update solitairePrime.cpp

--- solitairePrime.cpp
+++ solitairePrime.cpp
@@ -1,11 +1,3 @@
-// 	Chris Nguyen
-//	Class (CECS 282-04)
-//	Prog 1 – Solitaire Prime
-// 	9/14/2021
-// 
-//  I certify that this program is my own original work. I did not copy any part of this program from 
-//  any other source. I further certify that I typed each and every line of code in this program.
-
 #include <iostream>
 #include <string>
 #include "Card.h"
